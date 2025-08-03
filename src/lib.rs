c5f0a960855d1a7040117cdc683282377325d7a1
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-08-04 03:15:29 +0530

    init a contract, add doubling logic for counter

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
+
+entrypoint!(program_instruction);
+
+#[derive(BorshDeserialize, BorshSerialize)]
+struct OnChainData {
+    count: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn program_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo], // [data account]
+    intruction_data: &[u8],
+) -> ProgramResult {
+    let mut iter = accounts.iter();
+    let data_account = next_account_info(&mut iter)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let mut counter = OnChainData::try_from_slice(&data_account.data.borrow_mut())?; // [u8]
+
+    if counter.count == 0 {
+        counter.count = 1;
+    } else {
+        counter.count = counter.count * 2;
     }
+
+    counter.serialize(&mut *data_account.data.borrow_mut());
+
+    Ok(())
 }
